753d847fd9ceb4ef94630df4b9493821c54c474f
tomatoandcheerio <Georxia@gmail.com>
2017-05-18 19:57:34 -0400

    added blogs page and able to comment on blog post

--- app.rb
+++ app.rb
@@ -21,16 +21,63 @@ end
 
 get "/login" do
 
+erb :login
+end
+
+post "/login" do
+  user_name_given = params[:username]
+  password_given = params[:password]
+
+  user = User.where(username: user_name_given).first
+
+  if user.password == password_given
+    session[:user_id] = user.id
+
+    redirect "/profile"
+  else
+    redirect "/login"
+  end
+
+
+end
+
+get "/logout" do
+  session[:user_id] = nil
+  redirect '/login'
 
 end
 
 get "/profile" do
+  @blogs = User.find(session[:user_id]).blogs
 
 erb :profile
 end
 
+post "/profile" do
+content = params[:content]
+
+Blog.create(user_id: session[:user_id], content: content)
+
+redirect "/profile"
+end
+
+get "/blogpost/:id" do
+@user = User.find(session[:user_id]).username
+@blog_id = params[:id]
+@blog = Blog.find(@blog_id)
+
+@comments = @blog.comments
+erb :blogpost
+end
+
+post "/blogpost" do
+blog_id = params[:blog_id]
+p blog_id
+Comment.create(user_id: session[:user_id], blog_id: blog_id, acomment: params[:acomment])
 
 
+redirect "/blogpost/#{blog_id}"
+end
 get "/blog/:id" do
 
 erb :otherprofile
--- models.rb
+++ models.rb
@@ -1,6 +1,6 @@
 class Blog < ActiveRecord::Base
   belongs_to :user
-  has_many :comments 
+  has_many :comments
 
 end
 
