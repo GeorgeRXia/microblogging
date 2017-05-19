9afc986fda41e6382f5898e4066054f8fc44123a
tomatoandcheerio <Georxia@gmail.com>
2017-05-19 07:06:27 -0400

    edited an import syntax on styles.scss

--- models.rb
+++ models.rb
@@ -1,15 +1,13 @@
 class Blog < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
 end
 
 class User < ActiveRecord::Base
 
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
   has_many :comments
-
-
 
 end
 
